7d575088665fb4991a7c8708a4d51ca98aad3a1b
sonal-takalikar <takalikar.s@northeastern.edu>
2024-11-28 18:12:54 -0500

    Adding InPersonSession page

---
+++ frontend/components/InPersonSession.js
@@ -0,0 +1,13 @@
+// pages/inpersonsession.js
+import React from 'react';
+
+const InPersonSession = () => {
+    return (
+        <div>
+            <h1>Welcome to the In-Person Session</h1>
+            <p>Details about the in-person session.</p>
+        </div>
+    );
+};
+
+export default InPersonSession;
