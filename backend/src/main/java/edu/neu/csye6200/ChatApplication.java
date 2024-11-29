13c655d69d6efdc71f5e4693276fdc74a4fe8acb
sahanajayadevaprakash <jayadevaprakash.s@northeastern.edu>
2024-11-29 13:33:38 -0500

    ccommon entrypoint for driver and chat

--- backend/src/main/java/edu/neu/csye6200/ChatApplication.java
+++
@@ -1,9 +0,0 @@
-package edu.neu.csye6200;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-@SpringBootApplication
-public class ChatApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(ChatApplication.class, args);
-    }
-}
